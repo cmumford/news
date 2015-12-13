18484f94408a78aaf9f4d2ccba4c7c9a0619a7a6
Chris Mumford <cmumford@users.noreply.github.com>
2015-12-13 15:26:07 -0800

    More work.

--- npr.py
+++ npr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import copy
+import csv
 import datetime
 import glob
 import json
@@ -8,6 +9,9 @@ import re
 import sys
 import urllib
 import xml.etree.ElementTree
+import xml.etree.cElementTree as ET
+import itertools
+import dateutil.parser
 
 # Uses the NPR API: http://api.npr.org/
 # Query generator: http://www.npr.org/api/queryGenerator.php
@@ -37,9 +41,17 @@ class Tag(object):
   def __str__(self):
     return unicode(self).encode('utf-8')
 
+class Story(object):
+  def __init__(self, id):
+    self.id_ = id
+    self.title_ = ''
+    self.date_ = None
+    self.tags_ = []
+
 class NPR(object):
   baseUrl = 'http://api.npr.org/query?'
   cancer = False
+  all_tags = []
   female_tags = set()
   male_tags = set()
   female_stories = set()
@@ -141,7 +153,7 @@ class NPR(object):
     return dt.strftime('%Y-%m-%d')
 
   def downloadData(self):
-    params = {'startNum':30640, 'numResults':20}
+    params = {'startNum':154534, 'numResults':20}
     story_count = 1    # Any non-zero number to start
     total_stories = 0
     while story_count:
@@ -189,6 +201,46 @@ class NPR(object):
 
     return story_count
 
+  def loadStoriesFromFiles(self, all_tags, file_names):
+    tags = {}
+    for tag in all_tags:
+      tags[tag.id_] = tag
+    stories = []
+    start = datetime.datetime.now()
+    idx = 0
+    for fname in file_names:
+      idx += 1
+      elapsed = datetime.datetime.now() - start
+      files_per_sec = idx / elapsed.total_seconds()
+      percent = idx * 100.0 / len(file_names)
+      remaining_secs = (len(file_names) - idx) / files_per_sec
+      print '%s: %.1f%%, fps:%.1f, remaining:%ds' % \
+          (fname, percent, files_per_sec, remaining_secs)
+      root = xml.etree.ElementTree.parse(fname).getroot()
+      for xml_story in root.findall('list/story'):
+        story = Story(int(xml_story.get('id')))
+        story.title_ = xml_story.find('title').text
+        story.date_ = dateutil.parser.parse(xml_story.find('storyDate').text)
+        stories.append(story)
+        for parent in xml_story.findall("parent[@type='tag']"):
+          tag_id = int(parent.get('id'))
+          tag = tags[tag_id]
+          story.tags_.append(tag)
+    return stories
+
+  def loadStories(self, all_tags):
+    file_names = glob.glob('stories/*.xml')
+    return self.loadStoriesFromFiles(all_tags, file_names)
+
+  def loadMatchingStories(self, tags, all_tags):
+    stories = []
+    for story in self.loadStories(all_tags):
+      for tag in tags:
+        if tag in story.tags_:
+          stories.append(story)
+          break
+    return stories
+
   def countStories(self, tags):
     counts = {}
     ids_to_tag = {}
@@ -197,6 +249,7 @@ class NPR(object):
       ids_to_tag[tag.id_] = tag
     start = datetime.datetime.now()
     files = glob.glob('stories/*.xml')
+    files = files[:500]
     idx = 0
     for fname in files:
       idx += 1
@@ -220,28 +273,82 @@ class NPR(object):
               NPR.male_stories.add(story.get('id'))
     return counts
 
+  def writeStoriesToXml(self, stories):
+    root = ET.Element("nprml")
+    xml_list = ET.SubElement(root, "list")
+    for story in stories:
+      xml_story = ET.SubElement(xml_list, "story", id=str(story.id_))
+      ET.SubElement(xml_story, "title").text = story.title_
+      ET.SubElement(xml_story, "storyDate").text = str(story.date_)
+      for tag in story.tags_:
+        parent = ET.SubElement(xml_story, "parent", type='tag', id=str(tag.id_))
+        ET.SubElement(parent, "title").text = tag.title_
+
+    tree = ET.ElementTree(root)
+    tree.write("matching.xml")
+
+  @staticmethod
+  def loadTagsOfInterest():
+    NPR.all_tags = NPR.loadTags()
+    print 'There are', len(NPR.all_tags), 'total tags'
+    NPR.female_tags = NPR.femaleTags(NPR.all_tags)
+    print 'There are', len(NPR.female_tags), 'female tags'
+    NPR.male_tags = NPR.maleTags(NPR.all_tags)
+    print 'There are', len(NPR.male_tags), 'male tags'
+
+  # Extract a subset of the stories, and write them to a single file for
+  # analysis.
+  def extractMatchingStories(self):
+    NPR.loadTagsOfInterest()
+
+    combined_tags = [t for t in NPR.female_tags]
+    combined_tags.extend([t for t in NPR.male_tags])
+
+    matching_stories = npr.loadMatchingStories(combined_tags, NPR.all_tags)
+    print 'There are', len(matching_stories), 'matching stories'
+    npr.writeStoriesToXml(matching_stories)
+
+  @staticmethod
+  def one_in_another(container_a, container_b):
+    for c in container_a:
+      if c in container_b:
+        return True
+    return False
+
+  @staticmethod
+  def calcTagCounts(stories, tags):
+    totals = {}
+    for tag in tags:
+      totals[tag] = 0
+    for story in stories:
+      for tag in story.tags_:
+        if tag in totals:
+          totals[tag] = totals[tag] + 1
+    return totals
+
+  @staticmethod
+  def printDictAsCSV(d, fname):
+    the_dict = {tag.title_: d[tag] for tag in d}
+    with open(fname, 'wb') as f:
+      w = csv.DictWriter(f, the_dict)
+      w.writeheader()
+      w.writerow(the_dict)
+
+  def analyzeMatchingStories(self):
+    NPR.loadTagsOfInterest()
+    stories = self.loadStoriesFromFiles(NPR.all_tags, ['matching.xml'])
+    print 'There are', len(stories), 'matching'
+
+    female_counts = NPR.calcTagCounts(stories, NPR.female_tags)
+    NPR.printDictAsCSV(female_counts, 'analysis_female.xml')
+
+    male_counts = NPR.calcTagCounts(stories, NPR.male_tags)
+    NPR.printDictAsCSV(male_counts, 'analysis_male.xml')
+
+
 if __name__ == '__main__':
   api_key = open('key.txt').read().strip()
   npr = NPR(api_key)
-  tags = NPR.loadTags()
-  print 'There are', len(tags), 'total tags'
-  NPR.female_tags = NPR.femaleTags(tags)
-  print 'There are', len(NPR.female_tags), 'female tags'
-  NPR.male_tags = NPR.maleTags(tags)
-  print 'There are', len(NPR.male_tags), 'male tags'
 
-  counts = npr.countStories(tags)
-
-  female_count = 0
-  male_count = 0
-  for tag in counts:
-    if tag in NPR.female_tags:
-      female_count += counts[tag]
-    if tag in NPR.male_tags:
-      male_count += counts[tag]
-
-  print 'There are', female_count, 'stories with female tags'
-  print 'There are', male_count, 'stories with male tags'
-
-  print 'There are', len(NPR.female_stories), 'female stories'
-  print 'There are', len(NPR.male_stories), 'male stories'
+  #npr.extractMatchingStories()
+  npr.analyzeMatchingStories()
